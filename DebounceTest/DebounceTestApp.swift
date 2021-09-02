b61e5502aa5d915848ce9cf1678b8eac776d1ae7
Philip Dukhov <philip.dukhov@gmail.com>
2021-09-02 18:39:12 +0700

    Initial Commit

---
+++ DebounceTest/DebounceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebounceTestApp.swift
+//  DebounceTest
+//
+//  Created by Phil on 02.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DebounceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
